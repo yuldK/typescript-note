dcc6820c962a0d04cefd1e9ca240c16da8e3f9d3
yuldk <yul.dev.kim@gmail.com>
2020-12-17 02:17:05 +0900

    #0001 test fs
    - implement find folder method

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,46 @@
-const message: string = 'Hello World';
-console.log(message);
+import * as path from 'path'
+import * as fs from 'fs'
+
+function find_res(pos:string, res:string):string 
+{
+	while (true)
+	{
+		let target = path.join(pos, res);
+		try {
+			fs.accessSync(target, fs.constants.O_DIRECTORY);
+			return target;
+		} catch (err) {
+			let next = path.join(pos, '../');
+			if (pos == next)
+				return '';
+			pos = next;
+			continue;
+		}
+	}
+
+async function open_file(file) {
+	return fs.promises.access(file).then(function() {
+		return fs.promises.readFile(file);
+	}).catch(() => '');
+}
+
+function main()
+{
+	let dir = find_res(__dirname, 'res/');
+	if (dir !== '')
+	{
+		console.log(dir);
+		const file = open_file(dir + 'test.lst');
+		file.then(function (result) {
+			console.log('success open file');
+			console.log('-----------------');
+			console.log(result.toString());
+			console.log('-----------------');
+		}).catch(function (err) {
+			console.log('failure open file');
+			console.log(err);
+		});
+	}
+}
+
+main();
